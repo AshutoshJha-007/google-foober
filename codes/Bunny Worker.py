6c43efee5bb74b68c64967cf5b4c1d5060b63035
ASHUTOSH KUMAR JHA <95684293+AshutoshJha-007@users.noreply.github.com>
2023-12-18 20:15:10 +0530

    Delete codes directory

--- codes/Bunny Worker.py
+++
@@ -1,5 +0,0 @@
-def solution(x, y):
-    line = x + y - 1
-    start_line = sum(range(line))
-    worker_id = str(start_line + x)
-    return worker_id
--- codes/pg1.py
+++
@@ -1,4 +0,0 @@
-def solution(x, y):
-    symmetric_diff = list(set(x) ^ set(y))
-    result = symmetric_diff[0]
-    return result
